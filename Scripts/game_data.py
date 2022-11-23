da492a21140ea71b7368c5203888245bf2b51a63
Aditya Narendra <49760167+Ozzey@users.noreply.github.com>
2022-11-23 18:13:47 +0530

    Initial commit
    - Added basic code for the platformer game
    - Added player mechanics
    - Added overworld system
    - Added enemy mechanics
    - Added collision detection

---
+++ Scripts/game_data.py
@@ -0,0 +1,94 @@
+level_0 = {
+    'terrain': './graphics/levels/0/level_0._terrain.csv',
+    'coins': './graphics/levels/0/level_0._coins.csv',
+    'fg palms': './graphics/levels/0/level_0._props.csv',
+    'crates': './graphics/levels/0/level_0._crates.csv',
+    'enemies': './graphics/levels/0/level_0._enemies.csv',
+    'constraints': './graphics/levels/0/level_0._constraints.csv',
+    'player': './graphics/levels/0/level_0._player.csv',
+    'props': './graphics/levels/0/level_0._props.csv',
+    'node_pos': (110, 400),
+    'unlock': 1,
+    'node_graphics': './graphics/overworld/0'
+}
+
+level_1 = {
+    'terrain': './graphics/levels/0/level_0._terrain.csv',
+    'coins': './graphics/levels/0/level_0._coins.csv',
+    'fg palms': './graphics/levels/0/level_0._props.csv',
+    'crates': './graphics/levels/0/level_0._crates.csv',
+    'enemies': './graphics/levels/0/level_0._enemies.csv',
+    'constraints': './graphics/levels/0/level_0._constraints.csv',
+    'player': './graphics/levels/0/level_0._player.csv',
+    'props': './graphics/levels/0/level_0._props.csv',
+    'node_pos': (300,220),
+    'unlock': 2,
+    'node_graphics': './graphics/overworld/1'
+}
+
+level_2 = {
+    'terrain': './graphics/levels/0/level_0._terrain.csv',
+    'coins': './graphics/levels/0/level_0._coins.csv',
+    'fg palms': './graphics/levels/0/level_0._props.csv',
+    'crates': './graphics/levels/0/level_0._crates.csv',
+    'enemies': './graphics/levels/0/level_0._enemies.csv',
+    'constraints': './graphics/levels/0/level_0._constraints.csv',
+    'player': './graphics/levels/0/level_0._player.csv',
+    'props': './graphics/levels/0/level_0._props.csv',
+    'node_pos': (480,610),
+    'unlock': 3,
+    'node_graphics': './graphics/overworld/2'
+}
+
+level_3 = {
+    'terrain': './graphics/levels/0/level_0._terrain.csv',
+    'coins': './graphics/levels/0/level_0._coins.csv',
+    'fg palms': './graphics/levels/0/level_0._props.csv',
+    'crates': './graphics/levels/0/level_0._crates.csv',
+    'enemies': './graphics/levels/0/level_0._enemies.csv',
+    'constraints': './graphics/levels/0/level_0._constraints.csv',
+    'player': './graphics/levels/0/level_0._player.csv',
+    'props': './graphics/levels/0/level_0._props.csv',
+    'node_pos': (610,350),
+    'unlock': 4,
+    'node_graphics': './graphics/overworld/3'
+}
+
+level_4 = {
+    'terrain': './graphics/levels/0/level_0._terrain.csv',
+    'coins': './graphics/levels/0/level_0._coins.csv',
+    'fg palms': './graphics/levels/0/level_0._props.csv',
+    'crates': './graphics/levels/0/level_0._crates.csv',
+    'enemies': './graphics/levels/0/level_0._enemies.csv',
+    'constraints': './graphics/levels/0/level_0._constraints.csv',
+    'player': './graphics/levels/0/level_0._player.csv',
+    'props': './graphics/levels/0/level_0._props.csv',
+    'node_pos': (880,210),
+    'unlock': 5,
+    'node_graphics': './graphics/overworld/4'
+}
+
+level_5 = {
+    'terrain': './graphics/levels/0/level_0._terrain.csv',
+    'coins': './graphics/levels/0/level_0._coins.csv',
+    'fg palms': './graphics/levels/0/level_0._props.csv',
+    'crates': './graphics/levels/0/level_0._crates.csv',
+    'enemies': './graphics/levels/0/level_0._enemies.csv',
+    'constraints': './graphics/levels/0/level_0._constraints.csv',
+    'player': './graphics/levels/0/level_0._player.csv',
+    'props': './graphics/levels/0/level_0._props.csv',
+    'node_pos': (1050,400),
+    'unlock': 5,
+    'node_graphics': './graphics/overworld/5'
+}
+
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    3: level_3,
+    4: level_4,
+    5: level_5
+}
+
---
+++ main.py
@@ -0,0 +1,100 @@
+import pygame
+import sys
+from Scripts.settings import *
+from Scripts.level import Level
+from Scripts.overworld import Overworld
+from Scripts.support import read_data, write_data
+from Scripts.ui import UI
+
+
+class Game:
+    def __init__(self):
+        # stats
+        self.level = None
+        self.data = read_data()
+        self.max_level = self.data['max_level']
+        self.max_health = 100
+        self.current_health = 100
+        self.coins = 0
+
+        # audio
+        self.level_bg_music = pygame.mixer.Sound('./audio/level_music.wav')
+        self.overworld_bg_music = pygame.mixer.Sound('./audio/overworld_music.wav')
+
+        # overworld
+        self.overworld = Overworld(0, self.max_level, screen, self.create_level)
+        self.status = 'overworld'
+        self.overworld_bg_music.play(loops=-1)
+
+        # UI
+        self.ui = UI(screen)
+
+    def create_level(self, current):
+        self.level = Level(current, screen, self.create_overworld, self.increase_coins, self.change_health)
+        self.status = 'level'
+        self.overworld_bg_music.stop()
+        self.level_bg_music.play(loops=-1)
+
+    def create_overworld(self, current_level, new_max):
+        if new_max > self.max_level:
+            self.max_level = new_max
+            write_data('max_level', new_max)
+        self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
+        self.status = 'overworld'
+        self.overworld_bg_music.play(loops=-1)
+        self.level_bg_music.stop()
+
+    def increase_coins(self, amount):
+        self.coins += amount
+
+    def change_health(self, amount):
+        self.current_health += amount
+
+    def check_game_over(self):
+        if self.current_health <= 0:
+            self.current_health = 100
+            self.coins = 0
+            self.max_level = self.data['max_level']
+            self.overworld = Overworld(0, self.max_level, screen, self.create_level)
+            self.status = 'overworld'
+            self.level_bg_music.stop()
+            self.overworld_bg_music.play(loops=-1)
+
+    def run(self):
+        if self.status == 'overworld':
+            self.overworld.run()
+        else:
+            self.level.run()
+            self.ui.show_health(self.current_health, self.max_health)
+            self.ui.show_coins(self.coins)
+            self.check_game_over()
+
+
+# Pygame setup
+pygame.init()
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+game = Game()
+pygame.display.set_caption("CupHeads")
+
+top = pygame.image.load('./graphics/bg/1.png').convert()
+top = pygame.transform.scale(top, (screen_width, screen_height))
+middle = pygame.image.load('./graphics/bg/2.png').convert()
+middle = pygame.transform.scale(middle, (screen_width, screen_height))
+bottom = pygame.image.load('./graphics/bg/3.png').convert()
+bottom = pygame.transform.scale(bottom, (screen_width, screen_height))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('grey')
+    screen.blit(top, (0, 0))
+    screen.blit(middle, (0, 0))
+    screen.blit(bottom, (0, 0))
+    game.run()
+    pygame.display.update()
+    clock.tick(60)
